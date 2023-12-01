de1709ef3450d024e444b61eef8753ae24012dd3
JoaoVitorFarias <jvlopesfarias@gmail.com>
2023-12-01 13:24:04 -0300

    fix requiements

--- src/configuration/database.py
+++ src/configuration/database.py
@@ -1,8 +1,9 @@
+import os
 import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5435/users"
+DATABASE_URL = os.getenv('DATABASE_URL', "postgresql://postgres:postgres@db:5432/users")
 
 engine = _sql.create_engine(DATABASE_URL)
 
