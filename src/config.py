8c32477d797f7db5a8eb34c7576635bd251ec711
JoaoVitorFarias <jvlopesfarias@gmail.com>
2023-12-01 14:55:02 -0300

    config

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import os
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     db_connect_url: str = os.getenv('DATABASE_URL', "postgresql://postgres:postgres@db:5432/users").replace("://", "ql://", 1)
