81b5879ae4d00ca064f11c7b1222e0219cede982
JoaoVitorFarias <jvlopesfarias@gmail.com>
2023-12-08 15:21:19 -0300

    update config

--- src/database.py
+++ src/database.py
@@ -11,10 +11,10 @@ SQLALCHEMY_DATABASE_URL = settings.db_connect_url
 if not getenv("TEST"):
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL,
-        pool_size=200,
-        max_overflow=10,
+        pool_size=500,
+        max_overflow=250,
         pool_timeout=30,
-        pool_recycle=1800
+        pool_recycle=1000
     )
 else:
     engine = Mock() 
