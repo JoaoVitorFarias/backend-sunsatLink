87c266a98b49d750cc03d927a54903a3dc8a9084
JoaoVitorFarias <jvlopesfarias@gmail.com>
2023-12-13 14:29:02 -0300

    aaa

--- src/infrastructure/repositories/satellite_trajectory_repository.py
+++ src/infrastructure/repositories/satellite_trajectory_repository.py
@@ -16,8 +16,8 @@ class SatelliteTrajectoryRepository:
   def save(self, satellite_trajectory: SatelliteTrajectory) -> SatelliteTrajectory:
     
     satellite_trajectory_db = SatelliteTrajectoryDb()
-    satellite_trajectory_db.azimuth =  Numeric(asdecimal=False)(satellite_trajectory.azimuth)
-    satellite_trajectory_db.elevation =  Numeric(asdecimal=False)(satellite_trajectory.elevation)
+    satellite_trajectory_db.azimuth =  satellite_trajectory.azimuth
+    satellite_trajectory_db.elevation = satellite_trajectory.elevation
     satellite_trajectory_db.movimentation_command = satellite_trajectory.movimentation_command
     satellite_trajectory_db.time = satellite_trajectory.time
 
